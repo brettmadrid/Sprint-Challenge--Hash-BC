4a8a4771620c111f342591ee79535ae1a35b6a09
brettmadrid <brettmadrid9@gmail.com>
2019-10-13 18:38:08 -0700

    MVP

--- blockchain/miner.py
+++ blockchain/miner.py
@@ -25,7 +25,13 @@ def proof_of_work(last_proof):
 
     print("Searching for next proof")
     proof = 0
-    #  TODO: Your code here
+    # First convert the last_proof to a string
+    l_proof_string = f'{last_proof}'.encode()
+    # Then hash that string and pass to the valid_proof method
+    l_proof_hash = hashlib.sha256(l_proof_string).hexdigest()
+
+    while valid_proof(l_proof_hash, proof) is False:
+        proof += 1
 
     print("Proof found: " + str(proof) + " in " + str(timer() - start))
     return proof
@@ -39,8 +45,10 @@ def valid_proof(last_hash, proof):
     IE:  last_hash: ...AE9123456, new hash 123456888...
     """
 
-    # TODO: Your code here!
-    pass
+    guess = f'{proof}'.encode()
+    guess_hash = hashlib.sha256(guess).hexdigest()
+
+    return str(last_hash)[-6:] == guess_hash[:6]
 
 
 if __name__ == '__main__':
--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -16,8 +16,23 @@ def reconstruct_trip(tickets, length):
     hashtable = HashTable(length)
     route = [None] * length
 
-    """
-    YOUR CODE HERE
-    """
+    # insert tickets into hash table
+    for t in tickets:
+        hash_table_insert(hashtable, t.source, t.destination)
+
+    # look for ticket with key = "NONE" which is the first ticket
+    # store the value in current_ticket -> "PDX"
+    current_ticket = hash_table_retrieve(hashtable, "NONE")
+
+    while current_ticket is not "NONE": # to to the last ticket
+        for i in range(len(route)): 
+            route[i] = current_ticket # insert ticket into array
+
+            # Now take the value of what was just stored and use it as a key
+            # call retrieve again, passing this key in to get the next ticket in order
+            current_ticket = hash_table_retrieve(hashtable, current_ticket)
+            if current_ticket is "NONE": # if at last ticket
+                route[i+1] = current_ticket # increment index and insert at end
+                break
 
     return route
